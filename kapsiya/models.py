523296c50c3e1106dbe5264f7d2841b5faab2169
Julzpeter <chepngetichjuliet@gmail.com>
2023-09-22 12:06:06 +0300

    create the patient's table and migrate

--- kapsiya/models.py
+++ kapsiya/models.py
@@ -8,3 +8,22 @@ class Nurse(models.Model):
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20,null=True)
     status=models.BooleanField(default=False)  
+
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return "{} ({})".format(self.user.first_name)
+
+class Patient(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/PatientProfilePic/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    symptoms = models.CharField(max_length=100,null=False)
+    assignedDoctorId = models.PositiveIntegerField(null=True)
+    admitDate=models.DateField(auto_now=True)
+    status=models.BooleanField(default=False)
