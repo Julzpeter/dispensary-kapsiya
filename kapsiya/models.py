775503505f664480f0f4b127f66b5fd2c94bb88e
Julzpeter <chepngetichjuliet@gmail.com>
2023-09-22 12:12:43 +0300

    create the appointment table

--- kapsiya/models.py
+++ kapsiya/models.py
@@ -36,3 +36,12 @@ class Patient(models.Model):
         return self.user.id
     def __str__(self):
         return self.user.first_name+" ("+self.symptoms+")"
+
+class Appointment(models.Model):
+    patientId=models.PositiveIntegerField(null=True)
+    doctorId=models.PositiveIntegerField(null=True)
+    patientName=models.CharField(max_length=40,null=True)
+    doctorName=models.CharField(max_length=40,null=True)
+    appointmentDate=models.DateField(auto_now=True)
+    description=models.TextField(max_length=500)
+    status=models.BooleanField(default=False)
