4c39bd2fb763e064092e25018f548297f8f8596b
Julzpeter <chepngetichjuliet@gmail.com>
2023-09-23 12:08:24 +0300

    modify urls

--- kapsiya/urls.py
+++ kapsiya/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 urlpatterns=[
     path('', views.home_view, name="home_view"),
-    path('adminclick', views.adminclick_view),
+    path('adminclick/', views.adminclick_view),
 
 ]
