840aa699aebadef3dbe96d7ceb4bd8281e78de79
Julzpeter <chepngetichjuliet@gmail.com>
2023-09-23 13:57:34 +0300

    add more view function for the admin after login, logout urls

--- kapsiya/urls.py
+++ kapsiya/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views 
+from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns=[
     path('', views.home_view, name="home_view"),
     path('adminclick/', views.adminclick_view),
+    path('adminlogin/', views.sign_in, name='login'),
     path('adminsignup/', views.admin_signup_view, name='adminsignup'),
+    path('afterlogin/', views.afterlogin_view,name='afterlogin'),
+    path('logout/', LogoutView.as_view(template_name='index.html'),name='logout'),
 
 ]
--- kapsiya/views.py
+++ kapsiya/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import AdminSignUpForm, AdminLoginForm
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.http import HttpResponseRedirect
 # Create your views here.
 def home_view(request):
     if request.user.is_authenticated:
@@ -11,3 +14,62 @@ def adminclick_view(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('afterlogin')
     return render(request, 'adminclick.html')
+
+def sign_in(request):
+    if request.method == 'GET':
+        form = AdminLoginForm()
+        return render(request, 'adminlogin.html', {'form':form})
+
+    elif request.method == 'POST':
+        form = AdminLoginForm(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request,username=username,password=password)
+            if user:
+                login(request, user)
+                messages.success(request,f'Hi {username.title()}, welcome back!')
+                return HttpResponseRedirect('admin-dashboard')
+        
+        # form is not valid or user is not authenticated
+        messages.error(request,f'Invalid username or password')
+        return render(request,'adminlogin.html',{'form': form})
+
+def admin_signup_view(request):
+    if request.method == 'GET':
+        form = AdminSignUpForm()
+        return render(request, 'adminsignup.html', {'form': form})
+    if request.method == 'POST':
+        form = AdminSignUpForm(request.POST) 
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have singed up successfully.')
+            login(request, user)
+            return HttpResponseRedirect('adminlogin')
+        else:
+            return render(request, 'adminsignup.html', {'form': form})
+def is_admin(user):
+    return user.groups.filter(name='ADMIN').exists()
+def is_doctor(user):
+    return user.groups.filter(name='DOCTOR').exists()
+def is_patient(user):
+    return user.groups.filter(name='PATIENT').exists()
+
+def afterlogin_view(request):
+    if is_admin(request.user):
+        return redirect('admin-dashboard')
+    elif is_doctor(request.user):
+        accountapproval=models.Doctor.objects.all().filter(user_id=request.user.id,status=True)
+        if accountapproval:
+            return redirect('doctor-dashboard')
+        else:
+            return render(request,'doctor_wait_for_approval.html')
+    elif is_patient(request.user):
+        accountapproval=models.Patient.objects.all().filter(user_id=request.user.id,status=True)
+        if accountapproval:
+            return redirect('patient-dashboard')
+    else:
+        return render(request,'patient_wait_for_approval.html')
