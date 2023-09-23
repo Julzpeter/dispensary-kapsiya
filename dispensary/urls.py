7da1c2a13401e919b9bc3a3cf356f4647ccb2973
Julzpeter <chepngetichjuliet@gmail.com>
2023-09-23 11:52:28 +0300

    change the url pattern

--- dispensary/urls.py
+++ dispensary/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("kapsiya/", include("kapsiya.urls")),
+    path("", include("kapsiya.urls")),
     path('admin/', admin.site.urls),
     
 ]
