382e75fb7773e2124a77948e2beacc8c47de71f2
Julzpeter <chepngetichjuliet@gmail.com>
2023-09-23 12:00:38 +0300

    add view function for the adminclick

--- kapsiya/views.py
+++ kapsiya/views.py
@@ -5,3 +5,9 @@ def home_view(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('afterlogin')
     return render (request, 'index.html')
+
+##view function for showinng the signup/login button for the admin
+def adminclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request, 'adminclick.html')
