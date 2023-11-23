262ffe63938af8479c62a5e1d35aab1bfb5bf86b
Julzpeter <chepngetichjuliet@gmail.com>
2023-11-23 22:43:26 +0300

    make some modifications on styles

--- dispensary/settings.py
+++ dispensary/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
 import os
 from pathlib import Path
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
